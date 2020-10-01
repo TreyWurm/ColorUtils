2f94b6080348d2d93d7455e0b270f03bf53903dc
Trevor Rous <t.rous@citiesapps.com>
2020-10-01 13:58:52 +0200

    WIP overlaying background with alpha limit and blending multiple overlays

--- app/src/main/java/com/appmea/colorutils/example/MainActivity.kt
+++ app/src/main/java/com/appmea/colorutils/example/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.appmea.colorutils.example
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.ColorUtils
 import com.appmea.colorutils.MaterialColorUtils
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.tv_text).apply {
             setOnClickListener { }
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                background = colorUtils.createRippleSurface()
+                background = colorUtils.createRippleSurface(this)
             }
         }
     }
--- library/src/main/java/com/appmea/colorutils/MaterialColorUtils.kt
+++ library/src/main/java/com/appmea/colorutils/MaterialColorUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.StateListDrawable
+import android.view.View
 import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import com.google.android.material.color.MaterialColors
@@ -38,29 +39,110 @@ class MaterialColorUtils(val context: Context) {
         )
     }
 
-    fun createRippleSurface(): StateListDrawable? {
+    @ColorInt
+    fun getColorBackground(): Int {
+        return MaterialColors.getColor(
+            context, android.R.attr.colorBackground, ContextCompat.getColor(context, R.color.default_color_surface)
+        )
+    }
+
+    /**
+     * Use for SDK < 21, else use [com.appmea.colorutils.R.drawable.ripple_surface]
+     *
+     * Creates a Selector with default color [com.appmea.colorutils.R.attr.colorSurface] and pressed state color [com.appmea.colorutils.R.attr.colorRippleSurface]
+     * If those attributes are not defined, default colors will be used
+     *
+     * In case [com.appmea.colorutils.R.attr.colorSurface] is not fully opaque we need to merge with the first views/parents background color that is fully opaque
+     */
+    fun createRippleSurface(view: View): StateListDrawable? {
+        // We need an opaque background to overlay
+        // We are gonna check backgrounds for first opaque background color or resort to WHITE as default
+        var colorSurface = getColorSurface()
+        val colorViewBackground = view.getRGBBackgroundColor()
+        val colorBackground = getColorBackground()
+
+        var opaqueSurface = if (isOpaque(colorSurface)) {
+            colorSurface
+        } else if (colorViewBackground != null && isOpaque(colorViewBackground)) {
+            overlayToRgb(colorViewBackground, colorSurface)
+        }  else if (colorViewBackground != null && isOpaque(colorBackground)) {
+            overlayToRgb(colorBackground, colorSurface)
+        } else {
+            overlayToRgb(Color.WHITE, colorSurface)
+        }
+
+
         val selector = StateListDrawable()
         selector.addState(
             intArrayOf(android.R.attr.state_pressed),
-            ColorDrawable(rgbaToRGB(getColorSurface(), getColorRippleSurface()))
+            ColorDrawable(overlayToRgb(opaqueSurface, limitAlpha(getColorRippleSurface())))
         )
-        selector.addState(intArrayOf(), ColorDrawable(getColorSurface()))
+        selector.addState(intArrayOf(), ColorDrawable(colorSurface))
         return selector
     }
 
 
+    /**
+     * Returns an opaque color corresponding to overlaying the background with the overlay
+     *
+     * @param background Needs to be an opaque RGB color; otherwise it will be combined with solid white
+     * @param overlay (Transparent) overlay to
+     */
     @ColorInt
-    fun rgbaToRGB(@ColorInt background: Int, @ColorInt overlay: Int): Int {
+    fun overlayToRgb(@ColorInt background: Int, @ColorInt overlay: Int): Int {
+        var rgbBackground = background
+        // Need a RGB background without alpha to work, so we get a RGB by combining transparent background with a solid white
+        if ((0xFF and (rgbBackground shr 24)) < 255) {
+            rgbBackground = overlayToRgb(Color.WHITE, background)
+        }
         val alpha = (0xFF and (overlay shr 24)) / 255f
+
         val red = (0xFF and (overlay shr 16)) / 255f
         val green = (0xFF and (overlay shr 8)) / 255f
         val blue = (0xFF and overlay) / 255f
-        val redBackground = (0xFF and (background shr 16).toFloat().toInt()).toFloat()
-        val greenBackground = (0xFF and (background shr 8).toFloat().toInt()).toFloat()
-        val blueBackground = (0xFF and background.toFloat().toInt()).toFloat()
+
+        val redBackground = (0xFF and (rgbBackground shr 16)) / 255f
+        val greenBackground = (0xFF and (rgbBackground shr 8)) / 255f
+        val blueBackground = (0xFF and rgbBackground) / 255f
+
         val finalRed = (1 - alpha) * redBackground + alpha * red
         val finalGreen = (1 - alpha) * greenBackground + alpha * green
         val finalBlue = (1 - alpha) * blueBackground + alpha * blue
-        return Color.rgb((finalRed * 255f + 0.5f).toInt() shr 16, (finalGreen * 255f + 0.5f).toInt() shr 8, (finalBlue * 255f + 0.5f).toInt())
+
+        return Color.rgb((finalRed * 255f).toInt(), (finalGreen * 255f).toInt(), (finalBlue * 255f).toInt())
+    }
+
+
+    /**
+     * Returns either the background color of the view itself, if it is opaque, or the parents background color otherwise if defined
+     */
+    fun View.getRGBBackgroundColor(): Int? {
+        return when {
+            isOpaque((background as? ColorDrawable)?.color) -> (background as ColorDrawable).color
+            isOpaque(((parent as? View)?.background as? ColorDrawable)?.color) -> ((parent as View).background as ColorDrawable).color
+            else -> null
+        }
+    }
+
+    /**
+     * Limits the given colors alpha value to at most half
+     */
+    fun limitAlpha(@ColorInt int: Int): Int {
+        return if (Color.alpha(int) > 128) {
+            (int and 0x00FFFFFF) or -0x80000000
+        } else {
+            int
+        }
+    }
+
+    /**
+     * Checks if a color is fully opaque
+     */
+    fun isOpaque(@ColorInt int: Int?): Boolean {
+        return if (int == null) {
+            false
+        } else {
+            (0xFF and (int shr 24)) >= 255
+        }
     }
 }
